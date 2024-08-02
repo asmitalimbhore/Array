f99f3cf88c0d6e1dc492fd68361812eb48b9ce1d
Asmitalimbhore <asmitalimbhore@gmail.com>
2024-08-02 10:27:08 +0530

    first commit

---
+++ SumofArray.java
@@ -0,0 +1,11 @@
+public class SumofArray {
+    public static void main(String args[]){
+        int arr[] = {1,4,6,18,9};
+        int sum =0;
+        for(int i = 0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        System.out.println(sum);
+    }
+    
+}
